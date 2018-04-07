a0975c5bca12138c95b5d418753ca3bbdced6b84
Anthony Starks <ajstarks@gmail.com>
2018-04-06 23:54:39 -0400

    adjust sizes, add capgen script

--- code/gen2.go
+++ code/gen2.go
@@ -1,6 +1,5 @@
 // List
-items := []string{"First", "Second", "Third", 
-                  "Fourth", "Fifth"}
+items := []string{"First", "Second", "Third", "Fourth", "Fifth"}
 deck.StartSlide()
 deck.Text(10, 90, "Important Items", "sans", 5, "")
 deck.List(10, 70, 4, items, "bullet", "sans", "red")
