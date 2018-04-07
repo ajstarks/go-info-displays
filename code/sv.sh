6f2c45c828141338a4cb98bc264f93ac943115a1
Anthony Starks <ajstarks@gmail.com>
2018-04-06 20:47:56 -0400

    refactor shining and sv slides

--- code/sv.sh
+++ code/sv.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-opts="-csv -val=f -fulldeck=f -bottom=55"
+opts="-csv -val=f -fulldeck=f"
 echo '<deck>'
 for f in $*
 do
 	echo '<slide>'
 	symbol=$(basename $f .csv)
-	min=$(awk 'NR > 1 {print}' $f | sort -t, -k5 | head -1 | cut -d , -f 5)
+	min=$(awk 'NR > 1 {print}' $f | sort -t, -k5 -n | awk -F, 'END {printf "%.2f", $5}')
 	xlabel=$(awk 'END {printf "%d", NR/5}' $f)
-	dchart $opts -csvcol=Date,Close  -xlabel=0 -chartitle="$symbol Close/Volume" -bar=f -vol -hline=$min,Low:$min -yaxis $f
-	dchart $opts -csvcol=Date,Volume -xlabel=$xlabel -top=65  -bar -vol=f -color=gray -title=f $f
+	dchart $opts -bar=f -vol -dmin=t -csvcol=Date,Close  -xlabel=0 -chartitle="$symbol" -top=85 -bottom=60 -hline="$min,High($min)" $f
+	dchart $opts                     -csvcol=Date,Volume -xlabel=$xlabel                -top=58 -bottom=45 -color=gray -title=f     $f
 	echo '</slide>'
 done
 echo '</deck>'
