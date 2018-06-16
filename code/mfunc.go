2118b577f9c37fff58169e2853c3ec098d8cdc8e
Anthony Starks <ajstarks@gmail.com>
2018-06-16 09:18:56 -0400

    update mfunc

--- code/mfunc.go
+++ code/mfunc.go
@@ -11,19 +11,49 @@ type tfunc struct {
 	function func(float64) float64
 }
 
+func power(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
+
+func pow2(n float64) float64 {
+	return float64(power(2, int(n)))
+}
+
+func linear(x float64) float64 {
+	return x
+}
+
+func squared(x float64) float64 {
+	return x*x
+}
+
 func main() {
 	fname := flag.String("f", "sine", "function name")
 	xrange := flag.String("x", "0,6.283185,0.1", "x range")
 	flag.Parse()
 
 	var (
-		f tfunc
+		f     tfunc
 		xmin  = 0.0
 		xmax  = 2 * math.Pi
 		xstep = 0.1
 	)
 	fmt.Sscanf(*xrange, "%f,%f,%f", &xmin, &xmax, &xstep)
 	switch *fname {
+	case "square":
+		f = tfunc{"y=x*x", squared}
+	case "linear":
+		f = tfunc{"y=x", linear}
+	case "pow":
+		f = tfunc{"y=2^n", pow2}
 	case "e", "exp":
 		f = tfunc{"y=e(x)", math.Exp}
 	case "log":
@@ -41,7 +71,7 @@ func main() {
 		f = tfunc{"y=1", func(float64) float64 { return 1 }}
 	}
 	fmt.Printf("# %s\n", f.label)
-	for x := xmin; x < xmax; x += xstep {
+	for x := xmin; x <= xmax; x += xstep {
 		fmt.Printf("%.2f\t%.4f\n", x, f.function(x))
 	}
 }
