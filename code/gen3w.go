517abcc1e70032e8595f2effd0425d69bb5f591d
Anthony Starks <ajstarks@gmail.com>
2018-07-12 20:38:21 -0400

    update gen example

--- code/gen3w.go
+++ code/gen3w.go
@@ -1,7 +1,7 @@
 // Picture with text annotation
 quote := "Tony Stark was able to build this in a cave. With a box of scraps!"
 deck.StartSlide("black", "white")
-deck.Image(50, 50, 1920, 1080, "cave.jpg", "https://youtu.be/MtntTvuv8Aw")
-deck.Rect(70, 60, 60, 40, "black", 40)
+deck.Image(50, 50, 1600, 681, "cave.png", "https://youtu.be/g1fzMbAr1u0?t=253")
+deck.Rect(70, 60, 40, 40, "black", 40)
 deck.TextBlock(45, 70, quote, "sans", 5, 45, "")
 deck.EndSlide()
